1222475255842d3319c8fc77db1ad21af50a04c0
AlapanGhosh10 <alapanzico10@gmail.com>
2023-08-06 20:06:10 +0530

    setup done

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <header>
+            <p>CodeComposer</p>
+            <div id="log-sig">
+                <button>Login</button>
+                <button>Signup</button>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
